3764810b20387c52851d73d946108893af9454f5
Andre Torres <andre855@gmail.com>
2021-08-25 23:15:07 +0200

    adiciona comentarios

--- src/reverse-proxy.ts
+++ src/reverse-proxy.ts
@@ -1,12 +1,22 @@
 import http from "http";
 import { get } from "./http-client";
 
+/**
+ * Cria um mapeamento de uma rota para uma porta.
+ * Nesse caso esperamos que todas as rotas serão mapeadas localmente
+ */
 const portMap: Record<string, number> = {
     "/blog": 3030
 }
 
 const requestListener = (req: http.IncomingMessage, res: http.ServerResponse) => {
   
+    /**
+     * Busca no mapeamento se a rota atual esta mapeada para alguma porta. 
+     * Ele verifica o inicio da rota, então uma rota como:
+     *                    /blog/posts/webserver.ts 
+     * será mapeada para a porta 3030 como foi configurado em portMap. 
+     */
     const destination = Object.entries(portMap).find(entry => req.url?.startsWith(entry[0]));
 
     if (destination) {
